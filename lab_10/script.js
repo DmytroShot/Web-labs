6056203d7ce202eac1c42e5b9e51b6997591a5bc
DmytroShot <147613007+DmytroShot@users.noreply.github.com>
2024-11-11 09:41:59 +0200

    update lab_10

--- lab_10/script.js
+++ lab_10/script.js
@@ -1,8 +1,34 @@
 let container = document.getElementById("container");
-let data_arr={  "tank1":"a"
-                ,"tank2":"b"
-                ,"tank3":"c"
-                ,"tank4":"d"}
+let data_arr={
+    "tank1":{
+        "name":"A-43"
+        ,"hp":"200"
+        ,"dmg":"300"
+        ,"spd":"400"
+        ,"vrng":"500"
+    }
+    ,"tank2":{
+        "name":"2C-38"
+        ,"hp":"300"
+        ,"dmg":"400"
+        ,"spd":"500"
+        ,"vrng":"600"
+    }
+    ,"tank3":{
+        "name":"sturmtiger"
+        ,"hp":"400"
+        ,"dmg":"500"
+        ,"spd":"600"
+        ,"vrng":"700"
+    }
+    ,"tank4":{
+        "name":"M1-KVT"
+        ,"hp":"500"
+        ,"dmg":"600"
+        ,"spd":"700"
+        ,"vrng":"800"
+    }
+}
 
 if(container!=null){
     for (let i = 1; i<Object.keys(data_arr).length+1 ; i++) {
@@ -12,7 +38,7 @@ if(container!=null){
 
         img.id=i;
         img.src="images/tank"+i+".jpg";
-        txt.innerHTML=data_arr["tank"+i];
+        txt.innerHTML=data_arr["tank"+i]["name"];
 
         elem.append(img);
         elem.append(txt);
@@ -36,8 +62,13 @@ if(info_container!=null){
     let txt = document.createElement("h1");
     
     img.src="images/tank"+info_index+".jpg";
-    txt.innerHTML=data_arr["tank"+info_index];
-
+    let data_obj = data_arr["tank"+info_index];
+    for (const key in data_obj){
+        let p=document.createElement("p");
+        p.innerText=key+":"+data_obj[key];
+        txt.append(p)
+    }
+    
     elem.append(img);
     elem.append(txt);
     elem.classList.add("tank");
